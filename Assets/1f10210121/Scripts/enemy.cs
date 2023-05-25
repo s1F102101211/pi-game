bb75e32e70215aeba630d18b54f03f65b701c6b4
s1F102101211 <85672572+s1F102101211@users.noreply.github.com>
2023-05-25 22:39:19 +0900

    enemyの追加

--- Assets/1f10210121/Scripts/enemy.cs
+++ Assets/1f10210121/Scripts/enemy.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class enemy : MonoBehaviour
 {
+    private Animator anime;
+    float speed=3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anime=GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        anime.SetFloat("speed",speed);
+        transform.position-=new Vector3(speed*Time.deltaTime,0,0);
+        transform.localScale=new Vector3(2.795f,2.604f,1);
     }
 }
